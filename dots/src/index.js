833f5cf9e51bc443959ea6414831cf5a3a75c1b5
zhengyt1 <zhengyt1@shanghaitech.edu.cn>
2022-10-04 23:56:16 -0400

    feature: modify index.js (forget in the previous step)

--- dots/src/index.js
+++ dots/src/index.js
@@ -18,7 +18,7 @@ root.render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/registser" element={<Register />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/home" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
