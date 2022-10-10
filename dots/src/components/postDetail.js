cc83f55d58b128f6fa42813e82bace5030230978
zhengyt1 <zhengyt1@shanghaitech.edu.cn>
2022-10-09 20:14:13 -0400

    feature: add del/cancel to post

--- dots/src/components/postDetail.js
+++ dots/src/components/postDetail.js
@@ -2,7 +2,8 @@ import { Avatar } from "@mui/material";
 import Button from '@mui/material/Button';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
-
+import Chip from '@mui/material/Chip';
+import DeleteIcon from '@mui/icons-material/Delete';
 import './postDetail.css'
 
 function PostDetail(props) {
@@ -16,15 +17,26 @@ function PostDetail(props) {
     createdTime,
   } = props.postInfo;
   const username = "Tony Danzy";
+
+  const handleCancel = () => {
+    console.info('You clicked the Cancek.');
+  };
+
+  const handleDelete = () => {
+    console.info('You clicked the delete icon.');
+  };
+
   return (
     <div className="detail-container">
       <div className="left-part">
-        <img src={pic}/>
+        <img src={pic} alt="user-post" />
       </div>
       <div className="right-part">
         <div className="user-info">
           <Avatar />
           <div className="username">{username}</div>
+          <Chip label="Delete" variant="outlined" onClick={handleDelete} />
+          <Chip label="Cancel" variant="outlined" onClick={handleCancel} />
         </div>
         <hr />
         <div className="text">{text}</div>
