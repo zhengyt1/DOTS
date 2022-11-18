6c58c50b645d41a39a41122ea60da1aa74b1bc0a
xinyuanUpenn <zxy9815@seas.upenn.edu>
2022-11-18 15:55:45 -0500

    init mongodb and express server

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+const webapp = require('./server/testRouter');
+
+// (5) define the port
+const port = 8080;
+
+// start the server and connect to the DB
+webapp.listen(port, async () => {
+  console.log(`Server running on port: ${port}`);
+});
